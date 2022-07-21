863d199765ec2bfec7eeb59180407cf7dff360e3
yangguang151 <yangguang151@jd.com>
2022-07-21 14:20:29 +0800

    添加https支持

--- config/index.ts
+++ config/index.ts
@@ -1,17 +1,24 @@
-  
-const devTestProduce = true
-export let domain = devTestProduce ? 'http://dev.jr.jd.com:9999' :  `http://yangguang.natappvip.cc`
-export let requestHost: string = `${domain}/api`
-if (process.env.NODE_ENV === 'development') {
-	console.log('开发环境');
-	domain = `http://dev.jr.jd.com:9999`
-	requestHost = "http://dev.jr.jd.com:6789/api"
+export const devPort = '6789'
+export const proHost = '9999'
+export const devHost = 'dev.jr.jd.com'
+export let serveDomain = 'https://yangguang.natappvip.cc'
+// #ifdef H5
+serveDomain = window.location.origin;
+// #endif
+// #ifdef MP-WEIXIN
+serveDomain = `https://yangguang.natappvip.cc`;
+// #endif
+export let requestHost: string = `${serveDomain}/api`;
+if (process.env.NODE_ENV === "development") {
+    console.log("开发环境");
+    serveDomain = `http://${devHost}:${devPort}`;
+    requestHost = `http://${devHost}:${proHost}`;
 } else {
-	console.log('生产环境');
+    console.log("生产环境");
 }
-export const host: string = `${domain}`
-export const shopImgPath: string = `${domain}/image/shop`
-export const foodImgPath:string = `${domain}/image/food`
-export const commonImgPath:string = `${domain}/image/common`
-export const defaultFoodImg:string = `${domain}/image/food/default.png`
+export const host: string = `${serveDomain}`;
+export const shopImgPath: string = `${serveDomain}/image/shop`;
+export const foodImgPath: string = `${serveDomain}/image/food`;
+export const commonImgPath: string = `${serveDomain}/image/common`;
+export const defaultFoodImg: string = `${serveDomain}/image/food/default.png`;
 // export const requestHost: string = "http://dev.jr.jd.com:6789/api"
